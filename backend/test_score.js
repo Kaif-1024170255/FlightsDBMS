8ace88b0b9684aaf5cd5bafa968af08be7b92c7e
Kaif-1024170255 <valorant-kaif01@gmail.com>
2026-05-06 21:26:10 +0530

    esg

--- backend/test_score.js
+++
@@ -1,7 +0,0 @@
-async function check() {
-    const db = require('./config/db');
-    const [rows] = await db.execute('SELECT * FROM Sustainability_Score WHERE flight_id=1');
-    console.log(rows);
-    process.exit(0);
-}
-check();
